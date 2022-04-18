11ac44eb055fa335a330cabfa1ed30880fa9a0e8
Hess Wang <hess90wang@gmail.com>
2022-04-18 18:42:29 +0900

    exercise 2-2,2-3,3-1,3-2

---
+++ .history/exam2-3_20220418040944.c
@@ -0,0 +1,9 @@
+#include "2.h"
+/*사용자 헤더파일 2.h에 stdio.h 파일을 포함시켰으므로
+다시포함해 줄 필요 없음*/
+void main()
+{
+    int add_result, sub_result;
+    printf("10과 5를 더하면 %d이다. \n", ADD(10, 5));
+    printf("10에서 5를 빼면 %d이다. \n", SUB(10, 5));
+}
